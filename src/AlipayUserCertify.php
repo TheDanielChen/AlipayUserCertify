bb044754f9c38f58a377d61ea2046f7044ca8ae0
chenst <1103321658@qq.com>
2019-11-22 20:26:29 +0800

    add zmxy freeze pid

--- src/AlipayUserCertify.php
+++ src/AlipayUserCertify.php
@@ -364,7 +364,7 @@ class AlipayUserCertify
             'amount'                =>  $total_price,  //预授权金额
             'product_code'          =>  'PRE_AUTH_ONLINE', //固定
             //'payee_logon_id'        =>  '15307124426',
-            'payee_user_id'         =>  $config['pid'],
+            'payee_user_id'         =>  $config['pid'],  // payee_user_id  请传入  appid对应的pid
             'extra_param'           => '{"category":"TRAD_RENT_CAR"}',
 
         ];
